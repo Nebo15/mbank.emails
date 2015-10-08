03a844ca26fb87caff107500d3f74021c30487d5
Andrew Dryga <andrew@dryga.com>
2015-10-08 23:50:16 +0300

    New intercom template

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,52 @@
-var gulp      = require('gulp'),
-    inlineCss = require('gulp-inline-css');
+var gulp        = require('gulp'),
+    inline      = require('gulp-inline'),
+    inlineCSS   = require('gulp-inline-css'),
+    pngquant    = require('imagemin-pngquant'),
+    imagemin    = require('gulp-imagemin'),
+    browserSync = require('browser-sync'),
+    inlineimg   = require('gulp-inline-image');
 
-gulp.task('inlineCSS', function() {
-    return gulp.src('./src/*.html')
-        .pipe(inlineCss({
-                applyStyleTags: true,
-                applyLinkTags: true,
-                removeStyleTags: true,
-                removeLinkTags: true
+gulp.task('server', ['build', 'watch'], function() {
+    browserSync({
+        server: {
+            baseDir: 'build',
+            index: 'intercom.html'
+        },
+        files: ["build/**/*"],
+        port: 8080,
+        open: true,
+        notify: false,
+        ghostMode: false
+    });
+});
+
+gulp.task('imagemin', function() {
+    return gulp.src('src/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
         }))
-        .pipe(gulp.dest('./html'));
+        .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('build', ['imagemin'], function() {
+    return gulp.src('src/*.html')
+        .pipe(inline({
+            base: 'src/',
+            disabledTypes: ['css', 'js']
+        }))
+        .pipe(inlineCSS({
+            applyStyleTags: true,
+            applyLinkTags: true,
+            removeStyleTags: true,
+            removeLinkTags: true
+        }))
+        .pipe(gulp.dest('build/'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['src/*.css', 'src/*.html'], ['inlineCSS']);
+    gulp.watch(['src/**/*'], ['build']);
 });
 
-gulp.task('default',['watch'], function() {});
+gulp.task('default', ['server']);
